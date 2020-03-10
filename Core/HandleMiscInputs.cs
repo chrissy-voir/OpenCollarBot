7d669c278f49bf56ca2f7fcf74b48c9d922954cd
Zontreck <tarapiccari@gmail.com>
2020-03-10 13:05:56 -0700

    add back in the misc data handlers

--- Core/HandleMiscInputs.cs
+++ Core/HandleMiscInputs.cs
@@ -4,6 +4,8 @@ using System.Text;
 using Bot.CommandSystem;
 using Bot;
 using Bot.NonCommands;
+using OpenMetaverse;
+using OpenCollarBot.GroupCommands;
 
 
 namespace OpenCollarBot.Core
@@ -11,5 +13,53 @@ namespace OpenCollarBot.Core
     [NotCommand()]
     public class HandleMiscInputs : nCMD
     {
+        public void handle(string text, UUID User, string agentName, MessageHandler.Destinations src, UUID originator)
+        {
+            OCBotMemory ocb = OCBotMemory.Memory;
+
+            if (ocb.ActiveReportSessions.ContainsKey(User) && ocb.ActiveReportSessions.Count > 0)
+            {
+                // Send report response to GitCommands
+                GitCommands gc = new GitCommands();
+                gc.BugResponse(originator, User, ocb.ActiveReportSessions[User].ReportStage, text, src, BotSession.Instance.MHE, agentName);
+                return;
+            }
+
+            if (ocb.ActiveFeatureSessions.ContainsKey(User) && ocb.ActiveFeatureSessions.Count > 0)
+            {
+                GitCommands gc = new GitCommands();
+                gc.FeatureResponse(originator, User, ocb.ActiveFeatureSessions[User].ReportStage, text, src, BotSession.Instance.MHE, agentName);
+                return;
+            }
+
+            if (ocb.ActiveCommentSessions.ContainsKey(User) && ocb.ActiveCommentSessions.Count > 0)
+            {
+                GitCommands gc = new GitCommands();
+                gc.comment(originator, User, ocb.ActiveCommentSessions[User].ReportStage, text, src, BotSession.Instance.MHE, agentName);
+                return;
+            }
+
+            if (ocb.NoticeSessions.ContainsKey(User) && ocb.NoticeSessions.Count > 0)
+            {
+                GroupSystem gs = new GroupSystem();
+                gs.update_notice_sess(originator, User, text, src, BotSession.Instance.MHE, agentName);
+                return;
+            }
+
+            if (ocb.MailingLists.Count > 0)
+            {
+                // Scan all mailing lists for a session and agentKey that match.
+                foreach (string sML in ocb.MailingLists.Keys)
+                {
+                    OCBotMemory.MailList ML = ocb.MailingLists[sML];
+                    if (ML.PrepFrom == User && ML.PrepState == 1)
+                    {
+                        MailingLists.MailingLists cML = new MailingLists.MailingLists();
+                        cML.HandleMailListData(User, originator, src, BotSession.Instance.MHE, sML, text);
+                        return;
+                    }
+                }
+            }
+        }
     }
 }
