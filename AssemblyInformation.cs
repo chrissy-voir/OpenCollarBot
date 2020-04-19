8ed8da9129c50616b220279c4bc53bebf63d04d2
Zontreck <tara@zontreck.dev>
2020-04-19 16:15:35 -0700

    Fix issue: Octokit was not among the output DLLs, and !feature or !bug would fail silently

---
+++ AssemblyInformation.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System;
+
+[assembly: AssemblyCopyright("© 2020 Tara Piccari")]
+[assembly: AssemblyCompany("ZNI")]
+[assembly: AssemblyVersion("1.5.1.1607")]
+[assembly: AssemblyProduct("OpenCollarBot Plugin")]
+[assembly: AssemblyDescription("OpenCollarBot plugin for Core5")]
