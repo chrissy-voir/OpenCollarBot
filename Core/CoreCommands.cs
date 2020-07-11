1ff6e4db4bc6d5261cd8d73b178cb59fa4ac02b8
Zontreck <tara@zontreck.dev>
2020-07-10 20:47:46 -0700

    Fix usage text

--- Core/CoreCommands.cs
+++ Core/CoreCommands.cs
@@ -30,7 +30,7 @@ namespace OpenCollarBot
         }
 
 
-        [CommandGroup("lm", 0, 0, "open_collar_menu - Opens your OpenCollar menu", Destinations.DEST_LOCAL | Destinations.DEST_AGENT | Destinations.DEST_GROUP | Destinations.DEST_DISCORD)]
+        [CommandGroup("lm", 0, 0, "lm - Gives you a clickable link to the bot's location", Destinations.DEST_LOCAL | Destinations.DEST_AGENT | Destinations.DEST_GROUP | Destinations.DEST_DISCORD)]
         public void GetLandmark(UUID client, int level, string[] additionalArgs, Destinations source, UUID agentKey, string agentName)
         {
             Vector3 myPosition = BotSession.Instance.grid.Self.SimPosition;
